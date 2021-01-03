efa7557923fb5ad29781fb212422c4b64d710584
Vasco Nunes <contact@vasco.dev>
2021-01-03 02:50:10 +0000

    Open floaterm in current directory

--- repos/dotfiles/home/.config/nvim/lua/plugins/floaterm.lua
+++ repos/dotfiles/home/.config/nvim/lua/plugins/floaterm.lua
@@ -1,2 +1,2 @@
 vim.api.nvim_set_keymap('n', 'e', ':FloatermNew vifm<CR>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<F12>', ':FloatermToggle<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<F12>', ':FloatermToggle .<CR>', {noremap = true, silent = true})
