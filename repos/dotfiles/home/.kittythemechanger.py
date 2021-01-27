e076f3d2857755ee8d372fe1eead60ee57c1b4c9
Vasco Nunes <contact@vasco.dev>
2021-01-27 12:23:01 +0000

    Add kitty theme toggler support

--- repos/dotfiles/home/.kittythemechanger.py
+++ repos/dotfiles/home/.kittythemechanger.py
@@ -3,6 +3,6 @@ from pathlib import Path
 theme_dir = Path('~/.config/kitty/themes').expanduser()
 conf_dir = Path('~/.config/kitty').expanduser()
 theme_link = conf_dir.joinpath('theme.conf')
-light_theme_link = conf_dir.joinpath('gruvbox-light.conf')
-dark_theme_link = conf_dir.joinpath('gruvbox-dark.conf')
+light_theme_link = conf_dir.joinpath('light.conf')
+dark_theme_link = conf_dir.joinpath('dark.conf')
 socket = 'unix:/tmp/kittysocket'
