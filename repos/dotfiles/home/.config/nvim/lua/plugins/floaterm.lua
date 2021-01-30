2f9386ce012e4985f7b02a9ee1b4d0f81ef339bf
Vasco Nunes <contact@vasco.dev>
2021-01-30 16:06:02 +0000

    Change vifm keybinding

--- repos/dotfiles/home/.config/nvim/lua/plugins/floaterm.lua
+++ repos/dotfiles/home/.config/nvim/lua/plugins/floaterm.lua
@@ -1,2 +1,2 @@
-vim.api.nvim_set_keymap('n', '<leader>e', ':FloatermNew vifm<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', 'e', ':FloatermNew vifm<CR>', {noremap = true, silent = true})
 vim.api.nvim_set_keymap('n', '<leader>g', ':FloatermNew lazygit<CR>', {noremap = true, silent = true})
