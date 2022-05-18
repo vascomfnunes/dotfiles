dbfed49efd64c1be2abc729f95f581ea335fa8fe
Vasco Nunes <contact@vasco.dev>
2022-05-18 09:38:17 +0100

    Add ssh agent to fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,6 +1,14 @@
 if status is-interactive
   set -gx PATH $PATH ~/homebrew/bin
+
   export GPG_TTY=$(tty)
+
+  if test -z (pgrep ssh-agent | string collect)
+    eval (ssh-agent -c)
+    set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
+    set -Ux SSH_AGENT_PID $SSH_AGENT_PID
+  end
+
   source ~/.env_variables_fish
 end
 
