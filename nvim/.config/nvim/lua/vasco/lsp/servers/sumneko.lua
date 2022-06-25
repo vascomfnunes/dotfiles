6729c1d491ae15b24ec968a407b68dd1e3bf925a
Vasco Nunes <contact@vasco.dev>
2022-06-25 15:14:47 +0100

    Add sumneko lsp

--- nvim/.config/nvim/lua/vasco/lsp/servers/sumneko.lua
+++ nvim/.config/nvim/lua/vasco/lsp/servers/sumneko.lua
@@ -1,23 +1,25 @@
 local M = {}
 
+local sumneko_binary_path = vim.fn.exepath 'lua-language-server'
+local sumneko_root_path = vim.fn.fnamemodify(sumneko_binary_path, ':h:h:h')
+
+local runtime_path = vim.split(package.path, ';')
+
+table.insert(runtime_path, 'lua/?.lua')
+table.insert(runtime_path, 'lua/?/init.lua')
+
 M.settings = {
   Lua = {
     runtime = {
-      -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
       version = 'LuaJIT',
-      -- Setup your lua path
       path = runtime_path,
     },
     diagnostics = {
-      -- Get the language server to recognize the `vim` global
       globals = { 'vim' },
     },
     workspace = {
-      -- Make the server aware of Neovim runtime files
       library = vim.api.nvim_get_runtime_file('', true),
-      checkThirdParty = false,
     },
-    -- Do not send telemetry data containing a randomized but unique identifier
     telemetry = {
       enable = false,
     },
