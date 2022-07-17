89d11d08583bf222f7b2a369cfb7d837ca25b713
Vasco Nunes <contact@vasco.dev>
2022-07-17 11:43:01 +0100

    Update ufo configuration

--- nvim/.config/nvim/lua/vasco/ufo.lua
+++ nvim/.config/nvim/lua/vasco/ufo.lua
@@ -1,5 +1,45 @@
+local status_ok, ufo = pcall(require, 'ufo')
+
+if not status_ok then
+  return
+end
+
+vim.wo.foldcolumn = '0'
+vim.wo.foldlevel = 99
+vim.wo.foldenable = true
+vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+
+local handler = function(virtText, lnum, endLnum, width, truncate)
+  local newVirtText = {}
+  local suffix = ('  %d '):format(endLnum - lnum)
+
+  local targetWidth = width
+  local curWidth = 0
+
+  for _, chunk in ipairs(virtText) do
+    local chunkText = chunk[1]
+    local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+    if targetWidth > curWidth + chunkWidth then
+      table.insert(newVirtText, chunk)
+    else
+      chunkText = truncate(chunkText, targetWidth - curWidth)
+      local hlGroup = chunk[2]
+      table.insert(newVirtText, { chunkText, hlGroup })
+      chunkWidth = vim.fn.strdisplaywidth(chunkText)
+      if curWidth + chunkWidth < targetWidth then
+        suffix = suffix .. (' '):rep(targetWidth - curWidth - chunkWidth)
+      end
+      break
+    end
+    curWidth = curWidth + chunkWidth
+  end
+  table.insert(newVirtText, { suffix, 'MoreMsg' })
+  return newVirtText
+end
+
 require('ufo').setup {
   open_fold_hl_timeout = 0,
+  fold_virt_text_handler = handler,
   provider_selector = function(_, _, _)
     return { 'treesitter', 'indent' }
   end,
