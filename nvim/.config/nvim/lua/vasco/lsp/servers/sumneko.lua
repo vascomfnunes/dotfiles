3a3e75f885eca7e71a349b8bdde88d7db09b4cfc
Vasco Nunes <contact@vasco.dev>
2022-08-12 01:03:01 +0100

    Remove mini plugin

--- nvim/.config/nvim/lua/vasco/lsp/servers/sumneko.lua
+++ nvim/.config/nvim/lua/vasco/lsp/servers/sumneko.lua
@@ -1,25 +1,18 @@
 local M = {}
 
-local sumneko_binary_path = vim.fn.exepath 'lua-language-server'
-local sumneko_root_path = vim.fn.fnamemodify(sumneko_binary_path, ':h:h:h')
-
-local runtime_path = vim.split(package.path, ';')
-
-table.insert(runtime_path, 'lua/?.lua')
-table.insert(runtime_path, 'lua/?/init.lua')
-
 M.settings = {
   Lua = {
-    runtime = {
-      version = 'LuaJIT',
-      path = runtime_path,
-    },
     diagnostics = {
       globals = { 'vim' },
     },
     workspace = {
-      library = vim.api.nvim_get_runtime_file('', true),
+      library = {
+        [vim.fn.expand '$VIMRUNTIME/lua'] = true,
+        [vim.fn.expand '$VIMRUNTIME/lua/vim/lsp'] = true,
+      },
     },
+    maxPreload = 100000,
+    preloadFileSize = 10000,
     telemetry = {
       enable = false,
     },
