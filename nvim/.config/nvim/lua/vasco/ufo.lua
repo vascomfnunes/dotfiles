99f9ec4c53bd673a0720ca50c1d7b1fd9a54a07a
Vasco Nunes <contact@vasco.dev>
2022-08-12 14:01:25 +0100

    Fix fold symbol

--- nvim/.config/nvim/lua/vasco/ufo.lua
+++ nvim/.config/nvim/lua/vasco/ufo.lua
@@ -4,18 +4,18 @@ if not status_ok then
   return
 end
 
-vim.wo.foldcolumn = '0'
+vim.wo.foldcolumn = '2'
 vim.wo.foldlevel = 99
+vim.wo.foldnestmax = 0
 vim.wo.foldenable = true
 vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
 
 local handler = function(virtText, lnum, endLnum, width, truncate)
   local newVirtText = {}
   local suffix = ('  %d '):format(endLnum - lnum)
-
-  local targetWidth = width
+  local sufWidth = vim.fn.strdisplaywidth(suffix)
+  local targetWidth = width - sufWidth
   local curWidth = 0
-
   for _, chunk in ipairs(virtText) do
     local chunkText = chunk[1]
     local chunkWidth = vim.fn.strdisplaywidth(chunkText)
@@ -26,6 +26,7 @@ local handler = function(virtText, lnum, endLnum, width, truncate)
       local hlGroup = chunk[2]
       table.insert(newVirtText, { chunkText, hlGroup })
       chunkWidth = vim.fn.strdisplaywidth(chunkText)
+      -- str width returned from truncate() may less than 2nd argument, need padding
       if curWidth + chunkWidth < targetWidth then
         suffix = suffix .. (' '):rep(targetWidth - curWidth - chunkWidth)
       end
@@ -40,7 +41,23 @@ end
 require('ufo').setup {
   open_fold_hl_timeout = 0,
   fold_virt_text_handler = handler,
+  preview = {
+    win_config = {
+      border = { '', '─', '', '', '', '─', '', '' },
+      winhighlight = 'Normal:Folded',
+      winblend = 0,
+    },
+    mappings = {
+      scrollU = '<C-u>',
+      scrollD = '<C-d>',
+    },
+  },
   provider_selector = function(_, _, _)
     return { 'treesitter', 'indent' }
   end,
 }
+
+-- buffer scope handler
+-- will override global handler if it is existed
+local bufnr = vim.api.nvim_get_current_buf()
+require('ufo').setFoldVirtTextHandler(bufnr, handler)
