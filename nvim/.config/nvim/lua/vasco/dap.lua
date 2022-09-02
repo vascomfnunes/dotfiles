80ba299f459fd1eb0aecb82a86340c7ecb2f039b
Vasco Nunes <contact@vasco.dev>
2022-09-02 11:35:50 +0100

    Add ruby dap adapter

--- nvim/.config/nvim/lua/vasco/dap.lua
+++ nvim/.config/nvim/lua/vasco/dap.lua
@@ -8,11 +8,14 @@ if not status_ok then
 end
 
 require('dapui').setup()
-require('nvim-dap-virtual-text').setup({})
+require('nvim-dap-virtual-text').setup {}
 
-vim.fn.sign_define('DapBreakpoint', { text = ' ', texthl = 'LspDiagnosticsSignError', linehl = '', numhl = '' })
+vim.fn.sign_define('DapBreakpoint', { text = ' ', texthl = 'LspDiagnosticsSignError', linehl = '', numhl = '' })
 vim.fn.sign_define('DapStopped', { text = ' ', texthl = 'LspDiagnosticsSignInformation', linehl = '', numhl = '' })
-vim.fn.sign_define('DapBreakpointRejected', { text = ' ', texthl = 'LspDiagnosticSignWarning', linehl = '', numhl = '' })
+vim.fn.sign_define(
+  'DapBreakpointRejected',
+  { text = ' ', texthl = 'LspDiagnosticSignWarning', linehl = '', numhl = '' }
+)
 
 -- dap.set_log_level 'TRACE'
 
@@ -29,6 +32,12 @@ dap.adapters.node2 = {
   type = 'executable',
   command = 'node',
   args = { os.getenv 'HOME' .. '/.local/share/nvim/mason/packages/vscode-node-debug2/out/src/nodeDebug.js' },
+}
+
+dap.adapters.ruby = {
+  type = 'executable',
+  command = 'bundle',
+  args = { 'exec', 'readapt', 'stdio' },
 }
 
 dap.configurations.javascript = {
@@ -54,14 +63,15 @@ dap.configurations.typescript = {
     request = 'launch',
     name = 'Debug Jest Tests',
     runtimeExecutable = 'node',
-    runtimeArgs = {
-      './node_modules/jest/bin/jest.js',
-      '--runInBand',
-    },
+    runtimeArgs = { '--inspect-brk', '${workspaceFolder}/node_modules/.bin/jest' },
+    args = { '${file}', '--runInBand', '--coverage', 'false' },
     rootPath = '${workspaceFolder}',
     cwd = '${workspaceFolder}',
     console = 'integratedTerminal',
     internalConsoleOptions = 'neverOpen',
+    sourceMaps = true,
+    port = 9229,
+    skipFiles = { '<node_internals>/**', 'node_modules/**' },
   },
 }
 
@@ -96,5 +106,16 @@ dap.configurations.typescriptreact = {
     cwd = '${workspaceFolder}',
     console = 'integratedTerminal',
     internalConsoleOptions = 'neverOpen',
+  },
+}
+
+dap.configurations.ruby = {
+  {
+    type = 'ruby',
+    request = 'launch',
+    name = 'Rails',
+    program = 'bundle',
+    programArgs = { 'exec', 'rails', 's' },
+    useBundler = true,
   },
 }
