2f0b3c89dc0e011cf348a13cb79b5330b4440306
Vasco Nunes <contact@vasco.dev>
2022-09-02 12:14:42 +0100

    Add lua dap adapter

--- nvim/.config/nvim/lua/vasco/dap.lua
+++ nvim/.config/nvim/lua/vasco/dap.lua
@@ -30,6 +30,10 @@ vim.fn.sign_define(
 
 dap.defaults.fallback.terminal_win_cmd = '50vsplit new'
 
+dap.adapters.nlua = function(cb, config)
+  cb { type = 'server', host = config.host, port = config.port }
+end
+
 dap.adapters.node2 = {
   type = 'executable',
   command = 'node',
@@ -52,12 +56,25 @@ dap.adapters.chrome = {
 
 dap.configurations.javascript = {
   {
+    type = 'node2',
+    name = 'node attach',
+    request = 'attach',
+    program = '${file}',
+    cwd = vim.fn.expand '%:p:h',
+    sourceMaps = true,
+    protocol = 'inspector',
+  },
+  {
     type = 'chrome',
-    name = 'Javascript (Chrome)',
+    name = 'chrome',
     request = 'attach',
     program = '${file}',
     cwd = vim.fn.getcwd(),
-    sourceMaps = true,
+    sourceMapPathOverrides = {
+      -- Sourcemap override for nextjs
+      ['webpack://_N_E/./*'] = '${webRoot}/*',
+      ['webpack:///./*'] = '${webRoot}/*',
+    },
     protocol = 'inspector',
     port = 9222,
     webRoot = '${workspaceFolder}',
@@ -66,14 +83,27 @@ dap.configurations.javascript = {
 
 dap.configurations.typescript = {
   {
+    type = 'node2',
+    name = 'node attach',
+    request = 'attach',
+    program = '${file}',
+    cwd = vim.fn.expand '%:p:h',
+    sourceMaps = true,
+    protocol = 'inspector',
+  },
+  {
     type = 'chrome',
-    name = 'Typescript (Chrome)',
+    name = 'chrome',
     request = 'attach',
     program = function()
       return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
     end,
     cwd = vim.fn.getcwd(),
-    sourceMaps = true,
+    sourceMapPathOverrides = {
+      -- Sourcemap override for nextjs
+      ['webpack://_N_E/./*'] = '${webRoot}/*',
+      ['webpack:///./*'] = '${webRoot}/*',
+    },
     protocol = 'inspector',
     port = 9222,
     webRoot = '${workspaceFolder}',
@@ -82,14 +112,27 @@ dap.configurations.typescript = {
 
 dap.configurations.javascriptreact = {
   {
+    type = 'node2',
+    name = 'node attach',
+    request = 'attach',
+    program = '${file}',
+    cwd = vim.fn.expand '%:p:h',
+    sourceMaps = true,
+    protocol = 'inspector',
+  },
+  {
     type = 'chrome',
-    name = 'Javascript React (Chrome)',
+    name = 'chrome',
     request = 'attach',
     program = function()
       return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
     end,
     cwd = vim.fn.getcwd(),
-    sourceMaps = true,
+    sourceMapPathOverrides = {
+      -- Sourcemap override for nextjs
+      ['webpack://_N_E/./*'] = '${webRoot}/*',
+      ['webpack:///./*'] = '${webRoot}/*',
+    },
     protocol = 'inspector',
     port = 9222,
     webRoot = '${workspaceFolder}',
@@ -98,14 +141,27 @@ dap.configurations.javascriptreact = {
 
 dap.configurations.typescriptreact = {
   {
+    type = 'node2',
+    name = 'node attach',
+    request = 'attach',
+    program = '${file}',
+    cwd = vim.fn.expand '%:p:h',
+    sourceMaps = true,
+    protocol = 'inspector',
+  },
+  {
     type = 'chrome',
-    name = 'Typescript React (Chrome)',
+    name = 'chrome',
     request = 'attach',
     program = function()
       return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
     end,
     cwd = vim.fn.getcwd(),
-    sourceMaps = true,
+    sourceMapPathOverrides = {
+      -- Sourcemap override for nextjs
+      ['webpack://_N_E/./*'] = '${webRoot}/*',
+      ['webpack:///./*'] = '${webRoot}/*',
+    },
     protocol = 'inspector',
     port = 9222,
     webRoot = '${workspaceFolder}',
@@ -120,5 +176,22 @@ dap.configurations.ruby = {
     program = 'bundle',
     programArgs = { 'exec', 'rails', 's' },
     useBundler = true,
+  },
+}
+
+dap.configurations.lua = {
+  {
+    type = 'nlua',
+    request = 'attach',
+    name = 'Attach to running Neovim instance',
+    host = function()
+      local value = vim.fn.input 'Host [default: 127.0.0.1]: '
+      return value ~= '' and value or '127.0.0.1'
+    end,
+    port = function()
+      local val = tonumber(vim.fn.input 'Port: ')
+      assert(val, 'Please provide a port number')
+      return val
+    end,
   },
 }
