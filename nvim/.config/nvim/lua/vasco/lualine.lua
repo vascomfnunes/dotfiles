95457560f7523d20efa1f3e8fc231ab59018221e
Vasco Nunes <contact@vasco.dev>
2022-10-31 11:14:38 +0000

    Add lsp to statusline

--- nvim/.config/nvim/lua/vasco/lualine.lua
+++ nvim/.config/nvim/lua/vasco/lualine.lua
@@ -1,10 +1,64 @@
-require('lualine').setup {
+local lualine, status_ok = pcall(require, 'lualine')
+
+if not status_ok then
+  return
+end
+
+local colors = require 'colors'
+
+local config = {
+  options = {
+    component_separators = '',
+    section_separators = '',
+  },
   sections = {
     lualine_a = { 'mode' },
-    lualine_b = { 'branch', 'diff', 'diagnostics' },
+    lualine_b = { 'branch', 'diff' },
     lualine_c = { 'filename' },
     lualine_x = { 'encoding', 'filetype' },
     lualine_y = { 'progress' },
     lualine_z = { 'location' },
   },
 }
+
+-- Inserts a component in lualine_c at left section
+local function ins_left(component)
+  table.insert(config.sections.lualine_c, component)
+end
+
+ins_left {
+  'diagnostics',
+  sources = { 'nvim_diagnostic' },
+  symbols = { error = ' ', warn = ' ', info = ' ' },
+}
+
+-- Insert mid section. You can make any number of sections in neovim :)
+-- for lualine it's any number greater then 2
+ins_left {
+  function()
+    return '%='
+  end,
+}
+
+ins_left {
+  -- Lsp server name
+  function()
+    local msg = 'No Active Lsp'
+    local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
+    local clients = vim.lsp.get_active_clients()
+    if next(clients) == nil then
+      return msg
+    end
+    for _, client in ipairs(clients) do
+      local filetypes = client.config.filetypes
+      if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+        return client.name
+      end
+    end
+    return msg
+  end,
+  icon = ' LSP:',
+  color = { fg = colors.base0D },
+}
+
+require('lualine').setup(config)
