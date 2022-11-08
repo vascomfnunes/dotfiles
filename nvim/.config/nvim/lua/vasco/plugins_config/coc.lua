8c76d39b65352c1044ebe4807608d5905feaa357
Vasco Nunes <contact@vasco.dev>
2022-11-08 22:07:03 +0000

    Clean up

--- nvim/.config/nvim/lua/vasco/plugins_config/coc.lua
+++ nvim/.config/nvim/lua/vasco/plugins_config/coc.lua
@@ -79,12 +79,6 @@ keyset("n", "K", '<CMD>lua _G.show_docs()<CR>', { silent = true })
 
 -- Highlight the symbol and its references when holding the cursor.
 vim.api.nvim_create_augroup("CocGroup", {})
-vim.api.nvim_create_autocmd("CursorHold", {
-  group = "CocGroup",
-  command = "silent call CocActionAsync('highlight')",
-  desc = "Highlight symbol under cursor on CursorHold"
-})
-
 
 -- Symbol renaming.
 keyset("n", "<leader>r", "<Plug>(coc-rename)", { silent = true })
