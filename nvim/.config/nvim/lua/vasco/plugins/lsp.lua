0ceeb225203820afb77dfd834e604046df98c935
Vasco Nunes <contact@vasco.dev>
2023-03-08 01:09:52 +0000

    chore: move cmp plugins to its dependencies

--- nvim/.config/nvim/lua/vasco/plugins/lsp.lua
+++ nvim/.config/nvim/lua/vasco/plugins/lsp.lua
@@ -26,14 +26,18 @@ M.dependencies = {
   },
 
   -- Autocompletion
-  'hrsh7th/nvim-cmp',
-  'hrsh7th/cmp-buffer',
-  'hrsh7th/cmp-path',
-  'saadparwaiz1/cmp_luasnip',
-  'hrsh7th/cmp-nvim-lsp',
-  'hrsh7th/cmp-nvim-lua',
-  'hrsh7th/cmp-cmdline',
-  'onsails/lspkind.nvim',
+  {
+    'hrsh7th/nvim-cmp',
+    dependencies = {
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-path',
+      'saadparwaiz1/cmp_luasnip',
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-nvim-lua',
+      'hrsh7th/cmp-cmdline',
+      'onsails/lspkind.nvim',
+    },
+  },
 
   -- Snippets
   { 'L3MON4D3/LuaSnip', build = 'make install_jsregexp' },
