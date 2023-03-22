a8dfaedb0d93fd938c1e3397c8b4166f88c4a606
Vasco Nunes <contact@vasco.dev>
2023-03-22 00:46:06 +0000

    feat(fish): add aliases and exports

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,9 +2,68 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# Vim mode
 fish_vi_key_bindings
 
-alias c clear
-alias vi nvim
-alias ls 'exa --icons'
-alias la 'ls -la'
+alias vi='nvim'
+alias v='fd --type f --hidden --exclude .git --exclude .cache --exclude Library --exclude .local | fzf-tmux -p --reverse | xargs nvim'
+alias ls='exa --icons'
+alias kill_rails='kill -9 $(lsof -t -i:3000)'
+alias la='ls -la'
+alias c='clear'
+alias :q='exit'
+alias df='df -h'
+alias cat='bat'
+alias cp='cp -i'
+alias mv='mv -i'
+alias rm='rm -i'
+alias diff='diff --color=auto'
+alias grep="grep --color=auto --exclude-dir={.git,artwork,node_modules,vendor}"
+alias rspec='rspec -f d'
+alias music='ncmpcpp'
+alias yarn_interactive_upgrade='yarn upgrade-interactive --latest'
+
+# Git
+alias gP="git push"
+alias gc="git commit"
+alias gf="git fetch"
+alias gp="git pull"
+alias gr="git restore"
+alias gco="git checkout"
+alias gb="git branch"
+alias gs="git status"
+alias gl='git log'
+alias ga="git add"
+alias gd="git diff"
+alias lg="lazygit"
+alias gclean="git clean -f -d"
+
+# Tmux
+alias t="tmux -u"
+alias ta="tmux -u a"
+alias tls="tmux ls"
+alias tks="tmux kill-session -t"
+
+# Weechat (workaround for downloading script list under macOS)
+alias weechat="OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES weechat"
+
+# ctags
+# For now, I just care about Rails projects
+# where LSP is not so great for Gems&Co and tags deliver a much better job...
+alias ctags='ctags --tag-relative -R --sort=yes --languages=ruby,scss,javascript --exclude=.git --exclude=doc --exclude=coverage --exclude=log --exclude=public --exclude=tmp --exclude=node_modules . $(bundle list --paths)'
+
+# source environment variables that we do not want in source control
+source ~/.env_variables
+
+# Exports
+export EDITOR='nvim'
+export VISUAL='nvim'
+export PLAYER='mpv'
+export BROWSER_CLI='w3m'
+export PATH="/usr/local/sbin:$PATH"
+export XDG_CONFIG_HOME="$HOME/.config"
+export PATH="$HOME/bin:$PATH"
+
+# GPG
+export GPG_TTY=$(tty)
+
