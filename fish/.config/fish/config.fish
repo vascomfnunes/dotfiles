8f2eb61d8f2e2f14dcc93a3142bccfc0913e42dc
Vasco Nunes <contact@vasco.dev>
2023-03-22 20:53:37 +0000

    feat(fish): customize fish colours

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -8,6 +8,18 @@ fish_vi_key_bindings
 # Add homebrew to the path
 set -U fish_user_paths /opt/homebrew/bin/ $fish_user_paths
 
+# Exports
+export EDITOR='nvim'
+export VISUAL='nvim'
+export PLAYER='mpv'
+export BROWSER_CLI='w3m'
+export PATH="/usr/local/sbin:$PATH"
+export XDG_CONFIG_HOME="$HOME/.config"
+export PATH="$HOME/bin:$PATH"
+
+# GPG
+export GPG_TTY=$(tty)
+
 alias vi='nvim'
 alias v='fd --type f --hidden --exclude .git --exclude .cache --exclude Library --exclude .local | fzf-tmux -p --reverse | xargs nvim'
 alias ls='exa --icons'
@@ -58,15 +70,39 @@ alias ctags='ctags --tag-relative -R --sort=yes --languages=ruby,scss,javascript
 # source environment variables that we do not want in source control
 source ~/.env_variables
 
-# Exports
-export EDITOR='nvim'
-export VISUAL='nvim'
-export PLAYER='mpv'
-export BROWSER_CLI='w3m'
-export PATH="/usr/local/sbin:$PATH"
-export XDG_CONFIG_HOME="$HOME/.config"
-export PATH="$HOME/bin:$PATH"
+# Gruvbox material
+set -l foreground d4be98
+set -l selection 504945
+set -l comment 5b534d
+set -l red ea6962
+# set -l orange fe8019
+set -l yellow d8a657
+set -l green a9b665
+# set -l purple d3869b
+set -l cyan 89b482
+set -l blue 7daea3
 
-# GPG
-export GPG_TTY=$(tty)
+# Syntax Highlighting Colors
+set -g fish_color_normal $foreground
+set -g fish_color_command $cyan
+set -g fish_color_keyword $blue
+set -g fish_color_quote $yellow
+set -g fish_color_redirection $foreground
+set -g fish_color_end $orange
+set -g fish_color_error $red
+set -g fish_color_param $purple
+set -g fish_color_comment $comment
+set -g fish_color_selection --background=$selection
+set -g fish_color_search_match --background=$selection
+set -g fish_color_operator $green
+set -g fish_color_escape $blue
+set -g fish_color_autosuggestion $comment
 
+# Completion Pager Colors
+set -g fish_pager_color_progress $comment
+set -g fish_pager_color_prefix $cyan
+set -g fish_pager_color_completion $foreground
+set -g fish_pager_color_description $comment
+
+# Pure prompt
+set -g pure_color_git_branch purple
