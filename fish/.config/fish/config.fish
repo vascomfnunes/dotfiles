b39a4d8d47dfe78351717aee4f8acc0170979c40
Vasco Nunes <contact@vasco.dev>
2023-03-22 00:35:19 +0000

    feat: add initial fish shell configuration

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,10 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+fish_vi_key_bindings
+
+alias c clear
+alias vi nvim
+alias ls 'exa --icons'
+alias la 'ls -la'
