6099406323f517bc92cc1231cf96e8bd75c9d2fa
Vasco Nunes <contact@vasco.dev>
2023-05-05 01:04:02 +0100

    chore(nvim): clean up

--- nvim/.config/nvim/lua/vasco/autocommands.lua
+++ nvim/.config/nvim/lua/vasco/autocommands.lua
@@ -1,13 +1,13 @@
 local custom_group = vim.api.nvim_create_augroup('CustomCmdGroup', { clear = true })
 
 -- Highlight line on yank
--- vim.api.nvim_create_autocmd('TextYankPost', {
---   callback = function()
---     vim.highlight.on_yank { higroup = 'Visual', timeout = 200 }
---   end,
---   group = custom_group,
---   pattern = '*',
--- })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank { higroup = 'Visual', timeout = 200 }
+  end,
+  group = custom_group,
+  pattern = '*',
+})
 
 -- Trim whitespaces
 vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
@@ -22,19 +22,6 @@ vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
   pattern = { '*/node_modules/*' },
   command = 'lua vim.diagnostic.disable(0)',
 })
-
--- Cursorline only on active window
--- vim.api.nvim_create_autocmd({ 'BufEnter', 'WinEnter' }, {
---   group = custom_group,
---   pattern = { '*' },
---   command = 'setlocal cursorline',
--- })
---
--- vim.api.nvim_create_autocmd({ 'WinLeave' }, {
---   group = custom_group,
---   pattern = { '*' },
---   command = 'setlocal nocursorline',
--- })
 
 -- dont list quickfix buffers
 vim.api.nvim_create_autocmd({ 'FileType' }, {
