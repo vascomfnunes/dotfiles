c020ce987f564ffd5ba5dd4ff2c60eb7d9f59250
Vasco Nunes <contact@vasco.dev>
2023-05-07 23:26:56 +0100

    feat(nvim): add neotest colors
    Add colors for successful and failed tests, as well as for skipped and running
     tests. Also define colors for directories and files in the neotest output.

--- nvim/.config/nvim/lua/vasco/theme.lua
+++ nvim/.config/nvim/lua/vasco/theme.lua
@@ -3,6 +3,11 @@ vim.cmd 'colorscheme mini_gruvbox_material'
 
 -- Define colors for later use
 local gray = '#665c54'
+local green = '#a9b665'
+local red = '#ea6962'
+local yellow = '#d8a657'
+local blue = '#7daea3'
+local grey = '#dbae93'
 
 -- Define a function to simplify setting a highlight group
 local function set_hl(group, options)
@@ -18,3 +23,10 @@ set_hl('WinSeparator', { bg = 'NONE', fg = gray })
 set_hl('PmenuSel', { bg = gray })
 set_hl('PmenuThumb', { bg = gray })
 set_hl('WinBar', { bg = 'NONE' })
+set_hl('NeotestPassed', { fg = green })
+set_hl('NeotestFailed', { fg = red })
+set_hl('NeotestTarget', { fg = red })
+set_hl('NeotestSkipped', { fg = blue })
+set_hl('NeotestRunning', { fg = yellow })
+set_hl('NeotestDir', { fg = blue })
+set_hl('NeotestFile', { fg = grey })
