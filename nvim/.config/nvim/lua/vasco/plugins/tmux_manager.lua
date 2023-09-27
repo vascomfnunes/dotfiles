15408615e820e7ba8ebff601b1e3a5cacee7346e
Vasco Nunes <contact@vasco.dev>
2023-09-27 19:51:17 +0100

    fix(nvim): fix tmux manager keybindings

--- nvim/.config/nvim/lua/vasco/plugins/tmux_manager.lua
+++ nvim/.config/nvim/lua/vasco/plugins/tmux_manager.lua
@@ -7,12 +7,30 @@ return {
       use_icon = true,
       icon = ' ',
     }
-  end,
-  keys = {
-    {
-      '<leader>rg',
-      function()
-        require('tmux-awesome-manager.src.term').run_wk {
+
+    local tmux_term = require 'tmux-awesome-manager.src.term'
+    local wk = require 'which-key'
+
+    wk.register({
+      r = {
+        -- name = 'Rails',
+        R = tmux_term.run_wk {
+          cmd = 'bundle exec rails s',
+          name = 'Rails Server',
+          visit_first_call = false,
+          open_as = 'separated_session',
+          session_name = 'My Terms',
+        },
+        r = tmux_term.run_wk { cmd = 'bundle exec rails c', name = 'Rails Console', open_as = 'window' },
+        b = tmux_term.run_wk {
+          cmd = 'bundle install',
+          name = 'Bundle Install',
+          open_as = 'pane',
+          close_on_timer = 2,
+          visit_first_call = false,
+          focus_when_call = false,
+        },
+        g = tmux_term.run_wk {
           cmd = 'bundle exec rails generate %1',
           name = 'Rails Generate',
           questions = {
@@ -25,9 +43,22 @@ return {
               focus_when_call = false,
             },
           },
-        }
-      end,
-      desc = 'Generate',
-    },
-  },
+        },
+        d = tmux_term.run_wk {
+          cmd = 'bundle exec rails destroy %1',
+          name = 'Rails Destroy',
+          questions = {
+            {
+              question = 'Rails destroy: ',
+              required = true,
+              open_as = 'pane',
+              close_on_timer = 4,
+              visit_first_call = false,
+              focus_when_call = false,
+            },
+          },
+        },
+      },
+    }, { prefix = '<leader>', silent = true })
+  end,
 }
