70a05464be218efda0ac1c9ede9ff259a2c69031
Vasco Nunes <contact@vasco.dev>
2023-09-27 21:32:45 +0100

    feat(nvim): add autopairs cmp integration

--- nvim/.config/nvim/lua/vasco/plugins/auto_pairs.lua
+++ nvim/.config/nvim/lua/vasco/plugins/auto_pairs.lua
@@ -1,5 +1,12 @@
 return {
   'windwp/nvim-autopairs',
   event = 'InsertEnter',
-  opts = {},
+  config = function()
+    require('nvim-autopairs').setup()
+
+    --  cmp integration
+    local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
+    local cmp = require 'cmp'
+    cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+  end,
 }
