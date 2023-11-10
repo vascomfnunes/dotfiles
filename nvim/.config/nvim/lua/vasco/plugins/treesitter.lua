87e0f624b9235b0fea17eb1d30e18349b8d3dccf
Vasco Nunes <contact@vasco.dev>
2023-11-10 01:44:00 +0000

    fix(nvim): Disable treesitter textobject default mappings

--- nvim/.config/nvim/lua/vasco/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/vasco/plugins/treesitter.lua
@@ -8,7 +8,7 @@ return {
     {
       'chrisgrieser/nvim-various-textobjs',
       config = function()
-        require('various-textobjs').setup { useDefaultKeymaps = true }
+        require('various-textobjs').setup { useDefaultKeymaps = false }
       end,
     },
     'JoosepAlviste/nvim-ts-context-commentstring',
