1c57db436a431c933ef0af7793dc011ed573f17c
Vasco Nunes <contact@vasco.dev>
2024-06-07 11:56:27 +0100

    fix(nvim): Fix gptmodels keybindings

--- nvim/.config/nvim/lua/vasco/plugins/gptmodels.lua
+++ nvim/.config/nvim/lua/vasco/plugins/gptmodels.lua
@@ -5,17 +5,11 @@ return {
     'MunifTanjim/nui.nvim',
     'nvim-telescope/telescope.nvim',
   },
-  keys = {
-    {
-      '<leader>ac',
-      vim.cmd.GPTModelsCode,
-      desc = 'Code assistant',
-    },
+  config = function()
+    vim.api.nvim_set_keymap('v', '<leader>aa', ':GPTModelsCode<CR>', { noremap = true, desc = 'Code assistant' })
+    vim.api.nvim_set_keymap('n', '<leader>aa', ':GPTModelsCode<CR>', { noremap = true, desc = 'Code assistant' })
 
-    {
-      '<leader>aC',
-      vim.cmd.GPTModelsChat,
-      desc = 'Chat',
-    },
-  },
+    vim.api.nvim_set_keymap('v', '<leader>ar', ':GPTModelsChat<CR>', { noremap = true, desc = 'Chat' })
+    vim.api.nvim_set_keymap('n', '<leader>ac', ':GPTModelsChat<CR>', { noremap = true, desc = 'Chat' })
+  end,
 }
