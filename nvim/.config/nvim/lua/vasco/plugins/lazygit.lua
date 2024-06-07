8604d7f4b60ca46d2823bd6d4f679dec21630ede
Vasco Nunes <contact@vasco.dev>
2024-06-07 14:34:52 +0100

    feat(nvim): Add lazygit plugin

---
+++ nvim/.config/nvim/lua/vasco/plugins/lazygit.lua
@@ -0,0 +1,17 @@
+return {
+  "kdheepak/lazygit.nvim",
+  cmd = {
+    "LazyGit",
+    "LazyGitConfig",
+    "LazyGitCurrentFile",
+    "LazyGitFilter",
+    "LazyGitFilterCurrentFile",
+  },
+  -- optional for floating window border decoration
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  keys = {
+    { "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+  }
+}
