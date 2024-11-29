f3a0ad40a23b6ebfd524f82f5d79a04a88a23443
Vasco Nunes <vasco.m.nunes@me.com>
2024-11-29 22:58:51 +0000

    Update fish config and add One Dark theme
    This commit updates the Fish shell configuration and adds the One Dark
    theme for syntax highlighting.
    
    Changes:
    
    - Modified `fish/.config/fish/config.fish` to remove the Gruvbox
      material color settings and aliases for Foreman.
    - Updated `fish/.config/fish/fish_plugins` to include the
      `woheedev/onedark-fish` plugin.
    - Added a new file `fish/.config/fish/themes/One Dark.theme` with the
      One Dark theme settings for Fish shell.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -71,42 +71,5 @@ alias weechat="OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES weechat"
 # where LSP is not so great for Gems&Co and tags deliver a much better job...
 alias ctags='ctags --tag-relative -R --sort=yes --languages=ruby,scss,javascript --exclude=.git --exclude=doc --exclude=coverage --exclude=log --exclude=public --exclude=tmp --exclude=node_modules . $(bundle list --paths)'
 
-# Foreman
-alias foreman='foreman start -f $HOME/Profile.dev'
-
 # source environment variables that we do not want in source control
 source ~/.env_variables
-
-# Gruvbox material
-set -l foreground d4be98
-set -l selection 504945
-set -l comment 5b534d
-set -l red ea6962
-# set -l orange fe8019
-set -l yellow d8a657
-set -l green a9b665
-# set -l purple d3869b
-set -l cyan 89b482
-set -l blue 7daea3
-
-# Syntax Highlighting Colors
-set -g fish_color_normal $foreground
-set -g fish_color_command $cyan
-set -g fish_color_keyword $blue
-set -g fish_color_quote $yellow
-set -g fish_color_redirection $foreground
-set -g fish_color_end $orange
-set -g fish_color_error $red
-set -g fish_color_param $purple
-set -g fish_color_comment $comment
-set -g fish_color_selection --background=$selection
-set -g fish_color_search_match --background=$selection
-set -g fish_color_operator $green
-set -g fish_color_escape $blue
-set -g fish_color_autosuggestion $comment
-
-# Completion Pager Colors
-set -g fish_pager_color_progress $comment
-set -g fish_pager_color_prefix $cyan
-set -g fish_pager_color_completion $foreground
-set -g fish_pager_color_description $comment
