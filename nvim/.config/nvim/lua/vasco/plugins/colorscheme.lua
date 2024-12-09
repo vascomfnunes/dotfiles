8075d26596bd4fafc7e4070a8c4cd94013171703
Vasco Nunes <vasco.m.nunes@me.com>
2024-12-09 21:27:29 +0000

    Add diffview config
    Adds comprehensive keyboard mappings for diffview.
    
    Changes:
    
    - Configure detailed keymaps for diffview.nvim, including:
      - Navigation between files (<tab>, <s-tab>, [F, ]F)
      - File operations (gf, <C-w><C-f>, <C-w>gf)
      - Merge conflict resolution (<leader>gc{o,t,b,a}, dx)
      - Layout and panel controls (<leader>{e,b}, g<C-x>)

--- nvim/.config/nvim/lua/vasco/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/vasco/plugins/colorscheme.lua
@@ -14,26 +14,6 @@ return {
       transparent = true,
       dimInactive = false,
       terminalColors = true,
-      overrides = function(colors)
-        local theme = colors.theme
-        local makeDiagnosticColor = function(color)
-          local c = require 'kanagawa.lib.color'
-          return { fg = color, bg = c(color):blend(theme.ui.bg, 0.95):to_hex() }
-        end
-        return {
-          TelescopeTitle = { fg = theme.ui.special, bold = true },
-          TelescopePromptNormal = { bg = theme.ui.bg_p1 },
-          TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
-          TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
-          TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
-          TelescopePreviewNormal = { bg = theme.ui.bg_dim },
-          TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
-          DiagnosticVirtualTextHint = makeDiagnosticColor(theme.diag.hint),
-          DiagnosticVirtualTextInfo = makeDiagnosticColor(theme.diag.info),
-          DiagnosticVirtualTextWarn = makeDiagnosticColor(theme.diag.warning),
-          DiagnosticVirtualTextError = makeDiagnosticColor(theme.diag.error),
-        }
-      end,
       theme = 'wave', -- "wave", "dragon" or "lotus"
       background = {
         dark = 'wave',
@@ -72,6 +52,14 @@ return {
         vim.api.nvim_set_hl(0, 'WhichKeyDesc', { bg = 'none' })
         vim.api.nvim_set_hl(0, 'WhichKeyValue', { bg = 'none' })
         vim.api.nvim_set_hl(0, 'LazyNormal', { bg = 'none' })
+        vim.api.nvim_set_hl(0, 'TelescopeNormal', { bg = 'none' })
+        vim.api.nvim_set_hl(0, 'TelescopeBorder', { bg = 'none' })
+        vim.api.nvim_set_hl(0, 'TelescopePromptNormal', { bg = 'none' })
+        vim.api.nvim_set_hl(0, 'TelescopePromptBorder', { bg = 'none' })
+        vim.api.nvim_set_hl(0, 'TelescopeResultsNormal', { bg = 'none' })
+        vim.api.nvim_set_hl(0, 'TelescopeResultsBorder', { bg = 'none' })
+        vim.api.nvim_set_hl(0, 'TelescopePreviewNormal', { bg = 'none' })
+        vim.api.nvim_set_hl(0, 'TelescopePreviewBorder', { bg = 'none' })
       end,
     })
   end,
--- nvim/.config/nvim/lua/vasco/plugins/neogit.lua
+++ nvim/.config/nvim/lua/vasco/plugins/neogit.lua
@@ -2,7 +2,128 @@ local icons = require 'vasco.utils.icons'
 
 return {
   'NeogitOrg/neogit',
-  dependencies = { 'sindrets/diffview.nvim', 'nvim-lua/plenary.nvim' },
+  dependencies = {
+    {
+      'sindrets/diffview.nvim',
+      config = function()
+        local actions = require 'diffview.actions'
+        require('diffview').setup {
+          keymaps = {
+            view = {
+              {
+                'n',
+                '<tab>',
+                actions.select_next_entry,
+                { desc = 'Open the diff for the next file' },
+              },
+              {
+                'n',
+                '<s-tab>',
+                actions.select_prev_entry,
+                { desc = 'Open the diff for the previous file' },
+              },
+              {
+                'n',
+                '[F',
+                actions.select_first_entry,
+                { desc = 'Open the diff for the first file' },
+              },
+              {
+                'n',
+                ']F',
+                actions.select_last_entry,
+                { desc = 'Open the diff for the last file' },
+              },
+              {
+                'n',
+                'gf',
+                actions.goto_file_edit,
+                { desc = 'Open the file in the previous tabpage' },
+              },
+              { 'n', '<C-w><C-f>', actions.goto_file_split, { desc = 'Open the file in a new split' } },
+              { 'n', '<C-w>gf', actions.goto_file_tab, { desc = 'Open the file in a new tabpage' } },
+              { 'n', '<leader>e', actions.focus_files, { desc = 'Bring focus to the file panel' } },
+              { 'n', '<leader>b', actions.toggle_files, { desc = 'Toggle the file panel.' } },
+              {
+                'n',
+                'g<C-x>',
+                actions.cycle_layout,
+                { desc = 'Cycle through available layouts.' },
+              },
+              {
+                'n',
+                '[x',
+                actions.prev_conflict,
+                { desc = 'In the merge-tool: jump to the previous conflict' },
+              },
+              {
+                'n',
+                ']x',
+                actions.next_conflict,
+                { desc = 'In the merge-tool: jump to the next conflict' },
+              },
+              {
+                'n',
+                '<leader>gco',
+                actions.conflict_choose 'ours',
+                { desc = 'Choose the OURS version of a conflict' },
+              },
+              {
+                'n',
+                '<leader>gct',
+                actions.conflict_choose 'theirs',
+                { desc = 'Choose the THEIRS version of a conflict' },
+              },
+              {
+                'n',
+                '<leader>gcb',
+                actions.conflict_choose 'base',
+                { desc = 'Choose the BASE version of a conflict' },
+              },
+              {
+                'n',
+                '<leader>gca',
+                actions.conflict_choose 'all',
+                { desc = 'Choose all the versions of a conflict' },
+              },
+              { 'n', 'dx', actions.conflict_choose 'none', { desc = 'Delete the conflict region' } },
+              {
+                'n',
+                '<leader>gcO',
+                actions.conflict_choose_all 'ours',
+                { desc = 'Choose the OURS version of a conflict for the whole file' },
+              },
+              {
+                'n',
+                '<leader>gcT',
+                actions.conflict_choose_all 'theirs',
+                { desc = 'Choose the THEIRS version of a conflict for the whole file' },
+              },
+              {
+                'n',
+                '<leader>gcB',
+                actions.conflict_choose_all 'base',
+                { desc = 'Choose the BASE version of a conflict for the whole file' },
+              },
+              {
+                'n',
+                '<leader>gcA',
+                actions.conflict_choose_all 'all',
+                { desc = 'Choose all the versions of a conflict for the whole file' },
+              },
+              {
+                'n',
+                'dX',
+                actions.conflict_choose_all 'none',
+                { desc = 'Delete the conflict region for the whole file' },
+              },
+            },
+          },
+        }
+      end,
+    },
+    'nvim-lua/plenary.nvim',
+  },
   cmd = 'Neogit',
   keys = {
     { '<leader>gg', vim.cmd.Neogit, desc = 'Neogit' },
