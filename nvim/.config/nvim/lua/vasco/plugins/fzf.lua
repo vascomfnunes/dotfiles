fb88654424278466ab66ed617862ea59d50e0593
Vasco Nunes <vasco.m.nunes@me.com>
2024-12-18 21:27:26 +0000

    Replace neogit with fzf-lua git commands
    Removes neogit and its dependencies in favor of using fzf-lua's built-in git
    commands.
    
    Changes:
    
    - Remove `neogit.lua` and its diffview dependency
    - Add git commands to fzf-lua (`git_status`, `git_commits`, `git_branches`,
      `git_stash`)
    - Add navigation keybindings for fzf windows (<c-j>, <c-k>)
    - Configure fzf-lua quickfix keymaps for better UX
    - Update buffers command to sort by MRU and last used

--- nvim/.config/nvim/lua/vasco/plugins/fzf.lua
+++ nvim/.config/nvim/lua/vasco/plugins/fzf.lua
@@ -3,21 +3,41 @@ return {
   event = 'BufWinEnter',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   keys = {
+    { '<c-j>', '<c-j>', ft = 'fzf', mode = 't', nowait = true },
+    { '<c-k>', '<c-k>', ft = 'fzf', mode = 't', nowait = true },
     { 'z=', '<cmd>FzfLua spell_suggest<cr>', desc = 'Spell suggestions' },
     { '<leader>ff', '<cmd>FzfLua files<cr>', desc = 'Files' },
     { '<leader>fr', '<cmd>FzfLua resume<cr>', desc = 'Resume' },
     { '<leader>fc', '<cmd>FzfLua colorschemes<cr>', desc = 'Colorschemes' },
     { '<leader>fg', '<cmd>FzfLua live_grep<cr>', desc = 'Grep' },
     { '<leader>fG', '<cmd>FzfLua grep_cword<cr>', desc = 'Grep string' },
+    { '<leader>fv', '<cmd>FzfLua grep_visual<cr>', desc = 'Grep visual' },
     { '<leader>fd', '<cmd>FzfLua diagnostics_document<cr>', desc = 'LSP diagnostics' },
     { '<leader>fh', '<cmd>FzfLua helptags<cr>', desc = 'Help' },
     { '<leader>fq', '<cmd>FzfLua quickfix<cr>', desc = 'Quickfix' },
+    { '<leader>gg', '<cmd>FzfLua git_status<cr>', desc = 'Git status' },
+    { '<leader>gc', '<cmd>FzfLua git_commits<cr>', desc = 'Git commits' },
+    { '<leader>gb', '<cmd>FzfLua git_branches<cr>', desc = 'Git branches' },
+    { '<leader>gs', '<cmd>FzfLua git_stash<cr>', desc = 'Git stash' },
     {
       '<leader>fb',
-      '<cmd>FzfLua buffers<cr>',
+      '<cmd>FzfLua buffers sort_mru=true sort_lastused=true<cr>',
       desc = 'Buffers',
     },
   },
+  opts = function(_, opts)
+    local config = require 'fzf-lua.config'
+
+    -- Quickfix
+    config.defaults.keymap.fzf['ctrl-q'] = 'select-all+accept'
+    config.defaults.keymap.fzf['ctrl-u'] = 'half-page-up'
+    config.defaults.keymap.fzf['ctrl-d'] = 'half-page-down'
+    config.defaults.keymap.fzf['ctrl-x'] = 'jump'
+    config.defaults.keymap.fzf['ctrl-f'] = 'preview-page-down'
+    config.defaults.keymap.fzf['ctrl-b'] = 'preview-page-up'
+    config.defaults.keymap.builtin['<c-f>'] = 'preview-page-down'
+    config.defaults.keymap.builtin['<c-b>'] = 'preview-page-up'
+  end,
   config = function()
     local actions = require 'fzf-lua.actions'
 
