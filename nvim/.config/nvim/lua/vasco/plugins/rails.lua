c84bcea5b87f9547308fd966a1f0f482ec42d4f3
Vasco Nunes <vasco.m.nunes@me.com>
2024-12-22 01:12:13 +0000

    Clean up rails terminal setup and add guard support
    This refactoring simplifies the terminal setup for Rails development tools by:
    
    - Extracting repeated terminal configuration into `create_terminal` function
    - Using a table-based approach to define terminals and their commands
    - Generating global toggle functions dynamically using a loop
    - Adding `guard` support for test automation
    - Simplifying keymaps with concise table syntax
    - Removing redundant function declarations and comments

--- nvim/.config/nvim/lua/vasco/plugins/rails.lua
+++ nvim/.config/nvim/lua/vasco/plugins/rails.lua
@@ -8,116 +8,45 @@ return {
     config = function()
       local Terminal = require('toggleterm.terminal').Terminal
 
-      -- Create specific runners for Rails commands
-      local rails_server = Terminal:new {
-        cmd = 'bundle exec rails s',
-        direction = 'horizontal',
-        size = 15,
-        on_open = function(term)
-          vim.api.nvim_buf_set_keymap(term.bufnr, 'n', 'q', '<cmd>close<CR>', { noremap = true, silent = true })
-        end,
-      }
-
-      local rails_console = Terminal:new {
-        cmd = 'bundle exec rails c',
-        direction = 'horizontal',
-        size = 15,
-        on_open = function(term)
-          vim.api.nvim_buf_set_keymap(term.bufnr, 'n', 'q', '<cmd>close<CR>', { noremap = true, silent = true })
-        end,
-      }
-
-      local overmind = Terminal:new {
-        cmd = 'overmind s',
-        direction = 'horizontal',
-        size = 15,
-        on_open = function(term)
-          vim.api.nvim_buf_set_keymap(term.bufnr, 'n', 'q', '<cmd>close<CR>', { noremap = true, silent = true })
-        end,
-      }
-
-      local overmind_connect = Terminal:new {
-        cmd = 'overmind connect web',
-        direction = 'horizontal',
-        size = 15,
-        on_open = function(term)
-          vim.api.nvim_buf_set_keymap(term.bufnr, 'n', 'q', '<cmd>close<CR>', { noremap = true, silent = true })
-        end,
-      }
-
-      -- Functions to toggle specific terminals
-      function _RAILS_SERVER()
-        rails_server:toggle()
+      local function create_terminal(cmd)
+        return Terminal:new {
+          cmd = cmd,
+          direction = 'horizontal',
+          size = 15,
+          on_open = function(term)
+            vim.api.nvim_buf_set_keymap(term.bufnr, 'n', 'q', '<cmd>close<CR>', { noremap = true, silent = true })
+          end,
+        }
       end
 
-      function _RAILS_CONSOLE()
-        rails_console:toggle()
-      end
-      function _OVERMIND()
-        overmind:toggle()
-      end
-      function _OVERMIND_CONNECT()
-        overmind_connect:toggle()
+      local terminals = {
+        rails_server = create_terminal 'bundle exec rails s',
+        rails_console = create_terminal 'bundle exec rails c',
+        guard = create_terminal 'bundle exec guard',
+        overmind = create_terminal 'overmind s',
+        overmind_connect = create_terminal 'overmind connect web',
+      }
+
+      -- Create global toggle functions
+      for name, terminal in pairs(terminals) do
+        _G[string.upper(name)] = function()
+          terminal:toggle()
+        end
       end
     end,
     keys = {
-      -- Your existing mappings
-      {
-        '<leader>rc',
-        '<cmd>Econtroller<CR>',
-        desc = 'Controller',
-      },
-      {
-        '<leader>rv',
-        '<cmd>Eview<CR>',
-        desc = 'View',
-      },
-      {
-        '<leader>rh',
-        '<cmd>Ehelper<CR>',
-        desc = 'Helper',
-      },
-      {
-        '<leader>ra',
-        '<cmd>A<CR>',
-        desc = 'Alternate',
-      },
-      {
-        '<leader>rl',
-        '<cmd>Elocale<CR>',
-        desc = 'Locale',
-      },
-      {
-        '<leader>ru',
-        '<cmd>Eunittest<CR>',
-        desc = 'Unit test',
-      },
-      {
-        '<leader>rm',
-        '<cmd>Emodel<CR>',
-        desc = 'Model',
-      },
-      -- New mappings for Rails server and console
-      {
-        '<leader>rr',
-        '<cmd>lua _RAILS_SERVER()<CR>',
-        desc = 'Rails Server',
-      },
-      {
-        '<leader>ri',
-        '<cmd>lua _RAILS_CONSOLE()<CR>',
-        desc = 'Rails Console',
-      },
-      {
-        '<leader>ro',
-        '<cmd>lua _OVERMIND()<CR>',
-        desc = 'Start overmind',
-      },
-      {
-        '<leader>rC',
-        '<cmd>lua _OVERMIND_CONNECT()<CR>',
-        desc = 'Overmind connect web',
-      },
+      { '<leader>rc', '<cmd>Econtroller<CR>', desc = 'Controller' },
+      { '<leader>rv', '<cmd>Eview<CR>', desc = 'View' },
+      { '<leader>rh', '<cmd>Ehelper<CR>', desc = 'Helper' },
+      { '<leader>ra', '<cmd>A<CR>', desc = 'Alternate' },
+      { '<leader>rl', '<cmd>Elocale<CR>', desc = 'Locale' },
+      { '<leader>ru', '<cmd>Eunittest<CR>', desc = 'Unit test' },
+      { '<leader>rm', '<cmd>Emodel<CR>', desc = 'Model' },
+      { '<leader>rr', '<cmd>lua RAILS_SERVER()<CR>', desc = 'Rails Server' },
+      { '<leader>ri', '<cmd>lua RAILS_CONSOLE()<CR>', desc = 'Rails Console' },
+      { '<leader>ro', '<cmd>lua OVERMIND()<CR>', desc = 'Start overmind' },
+      { '<leader>rC', '<cmd>lua OVERMIND_CONNECT()<CR>', desc = 'Overmind connect web' },
+      { '<leader>rg', '<cmd>lua GUARD()<CR>', desc = 'Run Guard' },
     },
   },
 }
