059e99013b572c234e582c1731e674e8c6e843b9
Vasco Nunes <vasco.m.nunes@me.com>
2024-12-28 22:56:06 +0000

    perf: lazy load LuaSnip on insert mode entry
    Configure LuaSnip plugin to only load when entering insert mode rather
    than at startup. This improves initial Neovim startup time since the
    snippet engine is only loaded when actually needed.

--- nvim/.config/nvim/lua/vasco/plugins/luasnip.lua
+++ nvim/.config/nvim/lua/vasco/plugins/luasnip.lua
@@ -1,5 +1,6 @@
 return {
   'L3MON4D3/LuaSnip',
+  event = 'InsertEnter',
   build = (not jit.os:find 'Windows')
       and "echo 'NOTE: jsregexp is optional, so not a big deal if it fails to build'; make install_jsregexp"
     or nil,
