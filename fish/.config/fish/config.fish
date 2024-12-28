98da95ccd93da628550d22d5911b95c0cc38a1ab
Vasco Nunes <vasco.m.nunes@me.com>
2024-12-28 22:00:11 +0000

    refactor: migrate fish aliases to abbreviations
    Replace all aliases with fish native abbreviations for better performance and
    auto-expansion. Abbreviations expand on-the-fly and only take effect when
    typed, reducing shell startup time.
    
    Key changes:
    - Replace `alias` commands with `abbr -a`
    - Remove obsolete `invidtui` alias and comments
    - Fix command quoting style for consistency
    - Maintain same functionality while using fish's preferred mechanism

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -21,61 +21,58 @@ export PATH="$HOME/bin:$PATH"
 
 set -x GPG_TTY (tty)
 
-alias vi='nvim'
+# Aliases
 alias v='fd --type f --hidden --exclude .git --exclude .cache --exclude Library --exclude .local | fzf-tmux -p --reverse | xargs nvim'
-# https://github.com/darkhz/invidtui/wiki/Command-Line-Options
-# install: go install github.com/darkhz/invidtui@latest
-alias youtube='~/go/bin/invidtui --close-instances'
-#alias ls='eza --icons'
-alias kill_rails='kill -9 $(lsof -t -i:3000)'
-alias la='ls -la'
-alias c='clear'
-alias :q='exit'
-alias df='df -h'
-alias cat='bat'
-alias cp='cp -i'
-alias mv='mv -i'
-alias rm='rm -i'
-alias diff='diff --color=auto'
-alias grep="grep --color=auto --exclude-dir={.git,artwork,node_modules,vendor}"
-alias rspec='rspec -f d'
-alias music='ncmpcpp'
-alias yarn_interactive_upgrade='yarn upgrade-interactive --latest'
+
+# Abbreviations
+abbr -a -- vi 'nvim'
+abbr -a -- kill_rails 'kill -9 $(lsof -t -i:3000)'
+abbr -a -- la 'ls -la'
+abbr -a -- :q 'exit'
+abbr -a -- :cat 'bat'
+abbr -a -- cp 'cp -i'
+abbr -a -- mv 'mv -i'
+abbr -a -- rm 'rm -i'
+abbr -a -- diff 'diff --color=auto'
+abbr -a -- grep 'grep --color=auto --exclude-dir={.git,artwork,node_modules,vendor}'
+abbr -a -- rspec 'rspec -f d'
+abbr -a -- music 'ncmpcpp'
+abbr -a -- yarn_interactive_upgrade 'yarn upgrade-interactive --latest'
 
 # Git
-alias gP="git push"
-alias gc="git commit"
-alias gf="git fetch"
-alias gp="git pull"
-alias gr="git restore"
-alias gco="git checkout"
-alias gb="git branch"
-alias gs="git status"
-alias gl='git log'
-alias ga="git add"
-alias gd="git diff"
-alias lg="lazygit"
-alias gclean="git clean -f -d"
+abbr -a -- gP 'git push'
+abbr -a -- gc 'git commit'
+abbr -a -- gf 'git fetch'
+abbr -a -- gp 'git pull'
+abbr -a -- gr 'git restore'
+abbr -a -- gc 'git checkout'
+abbr -a -- gb 'git branch'
+abbr -a -- gs 'git status'
+abbr -a -- gl 'git log'
+abbr -a -- ga 'git add'
+abbr -a -- gd 'git diff'
+abbr -a -- lg 'lazygit'
+abbr -a -- gclean 'git clean -f -d'
 
 # Tmux
-alias t="tmux -u"
-alias ta="tmux -u a"
-alias tls="tmux ls"
-alias tks="tmux kill-session -t"
+abbr -a -- t 'tmux -u'
+abbr -a -- ta 'tmux -u a'
+abbr -a -- tls 'tmux ls'
+abbr -a -- tks 'tmux kill-session -t'
 
 # Weechat (workaround for downloading script list under macOS)
-alias weechat="OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES weechat"
+abbr -a -- weechat 'OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES weechat'
 
 # ctags
 # For now, I just care about Rails projects
 # where LSP is not so great for Gems&Co and tags deliver a much better job...
-alias ctags='ctags --tag-relative -R --sort=yes --languages=ruby,scss,javascript --exclude=.git --exclude=doc --exclude=coverage --exclude=log --exclude=public --exclude=tmp --exclude=node_modules . $(bundle list --paths)'
+abbr -a -- ctags 'ctags --tag-relative -R --sort=yes --languages=ruby,scss,javascript --exclude=.git --exclude=doc --exclude=coverage --exclude=log --exclude=public --exclude=tmp --exclude=node_modules . $(bundle list --paths)'
 
 # Yazi
-alias y="yazi"
+abbr -a -- y 'yazi'
 
 # Lazygit
-alias l="lazygit"
+abbr -a -- l 'lazygit'
 
 # source environment variables that we do not want in source control
 source ~/.env_variables
